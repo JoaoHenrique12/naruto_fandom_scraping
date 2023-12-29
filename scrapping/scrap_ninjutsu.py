6e201d066566d752cd1361de35e2b4ddbe47cffa
Joao Henrique <joao.henrique1299@hotmail.com>
2023-12-29 20:39:52 -0300

    (mess) test jutsu pages nested with main code. Time to start use pytest.

--- scrapping/scrap_ninjutsu.py
+++ scrapping/scrap_ninjutsu.py
@@ -1,12 +1,13 @@
+from time import sleep
 import requests
 import string
 
 from bs4 import BeautifulSoup
 
-def get_jutsu_information_then_process(jutsu_title, page_content):
-    soup = BeautifulSoup(page_content, 'html.parser')
-    wrappers = soup.find_all(lambda tag: tag.has_attr('data-source'))
+from database_connector import DataBase
 
+def process_jutsu(jutsu_title, soup):
+    wrappers = soup.find_all(lambda tag: tag.has_attr('data-source'))
     try:
         jutsu_image = soup.find("a", attrs={'class':"image image-thumbnail"})['href'] # type: ignore
     except TypeError:
@@ -20,9 +21,19 @@ def get_jutsu_information_then_process(jutsu_title, page_content):
     except IndexError:
         jutsu_range = ranges[0]
 
-    print(jutsu_title)
-    print(jutsu_image)
-    print(jutsu_range)
+    values = [jutsu_title, jutsu_range, jutsu_image]
+    with DataBase() as db:
+        query = "insert into jutsu (title, range_jutsu, image) values " + db.elements_to_string(values) + ';'
+        db.execute(query)
+
+def get_jutsu_information_then_process(jutsu_title, page_content):
+    soup = BeautifulSoup(page_content, 'html.parser')
+    wrappers = soup.find_all(lambda tag: tag.has_attr('data-source'))
+
+    process_jutsu(jutsu_title, soup)
+
+    wrappers = [w['data-source'] for w in wrappers]
+    print(*wrappers, sep='\n')
 
 
 if __name__ == "__main__":
@@ -32,6 +43,20 @@ if __name__ == "__main__":
         'Accept': 'text/html'
     }
 
+    test_jutsu = [
+        ('https://naruto.fandom.com/pt-br/wiki/A_Heran%C3%A7a_da_Vontade_do_Fogo', 'A Herança da Vontade do Fogo'),# Elementos, usuarios, C1, C2, Rank, Used For
+        ('https://naruto.fandom.com/pt-br/wiki/Ataque_de_Rel%C3%A2mpago', 'Ataque de Relâmpago'), # Nomes do jutsu, selos manuais
+        ('https://naruto.fandom.com/pt-br/wiki/Caminho_Humano', 'Caminho Humano'), # Jutsos relacionados
+        ('https://naruto.fandom.com/pt-br/wiki/F%C3%B3rmula_do_Selo_de_Oito_Trigramas', 'Fórmula do Selo de Oito Trigramas'), # Jutsus derivados
+    ]
+    for t in test_jutsu:
+        jutsu_page_content = requests.get(t[0], headers=headers).content # type: ignore
+        jutsu_title = t[1]
+        get_jutsu_information_then_process(jutsu_title, jutsu_page_content)
+        print("___________________________________________________________________")
+        sleep(1)
+
+    '''
     for l in string.ascii_uppercase:
         page_link = link_root + f'/pt-br/wiki/Categoria:Ninjutsu?from={l}'
         page = requests.get(page_link, headers=headers)
@@ -48,3 +73,4 @@ if __name__ == "__main__":
             get_jutsu_information_then_process(jutsu_title, jutsu_page_content)
             break
         break
+    '''
