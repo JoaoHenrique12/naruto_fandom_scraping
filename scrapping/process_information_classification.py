688ed5f9b0cd46c762e4e01fc4969f387583ad10
Joao Henrique <joao.henrique1299@hotmail.com>
2024-01-06 01:17:57 -0300

    (TDD) refactoring process_classification_c1_c2, done.

--- scrapping/process_information_classification.py
+++ scrapping/process_information_classification.py
@@ -2,20 +2,27 @@ from .utils import clean_string, load_info_jutsu
 from .database_connector import DataBase
 
 def process_classification_c1_c2(jutsu_title, soup):
+    c1 = None
+    c2 = None
+
+    wrappers = soup.find_all(lambda tag: tag.has_attr('data-source'))
     main_info = load_info_jutsu(jutsu_title)
-    c1 = []
-    c2 = []
-    if jutsu_title == 'A Herança da Vontade do Fogo':
-        c1 = [ 'Ninjutsu', 'Taijutsu']
-        c2 = ['Kekkei Genkai', 'Boujutsu', 'Bukijutsu', 'Kinjutsu', 'Konbijutsu', 'Ninjutsu Espaço-Tempo' ]
 
-    if jutsu_title == 'Ataque de Relâmpago':
-             c1, c2 = ['Ninjutsu'], []
-    if jutsu_title == 'Caminho Humano':
-             c1, c2 = [ 'Ninjutsu' ], ['Kekkei Genkai', 'Doujutsu']
+    try:
+        links_c1 = list(filter(lambda x: x['data-source'] == 'Classificação', wrappers))[0].div
+        c1 = [n.text for n in links_c1.find_all("a", recursive=False)]
+        c1 = list(map(clean_string, c1))
+        c1 = list(filter(lambda x: x in main_info['c1'].keys(), c1))
+    except IndexError:
+        c1 = []
 
-    c1 = list(map(clean_string, c1))
-    c2 = list(map(clean_string, c2))
+    try:
+        links_c2 = list(filter(lambda x: x['data-source'] == 'Classificação 2', wrappers))[0].div
+        c2 = [n.text for n in links_c2.find_all("a", recursive=False)]
+        c2 = list(map(clean_string, c2))
+        c2 = list(filter(lambda x: x in main_info['c2'].keys(), c2))
+    except IndexError:
+        c2 = []
 
 
     with DataBase() as db:
